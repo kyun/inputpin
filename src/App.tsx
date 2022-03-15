98dd0a5493d766f6b80b0380316655bcb896c4cd
Kyun <kyun@hpcnt.com>
2022-03-15 23:22:44 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import './App.css';
 import InputPin from './components/InputPin';
 
 function App() {
+  const [value, setValue] = React.useState('');
+    const [value2, setValue2] = React.useState('');
+
   return (
     <div className="App">
+      <section>
+        <h3>Skeleton</h3>
+        <InputPin value={value} setValue={setValue} id={'default'} />
+                <p>{value}</p>
 
-      <InputPin className={'input1'} />
-      <InputPin className={'input2'}   />
-      <InputPin className={'input3'} inputStyle={{ width: '30px' }}  />
-
-
+      </section>
+      <section style={{background: '#2177C7'}}>
+        <h2 style={{color: 'white'}}>style w/ className</h2>
+        <InputPin value={value2} setValue={setValue2} id={'input1'} className={'input1'} />
+        <p>{value2}</p>
+      </section>
     </div>
   );
 }
--- src/components/InputPin.tsx
+++ src/components/InputPin.tsx
@@ -1,49 +1,91 @@
 import React from 'react';
 
-interface Props{
+interface Props {
+  type?: string;
   length?: number;
   className?: string;
   inputStyle?: React.CSSProperties;
+  id?: string;
+  value?: string;
+  setValue?: (v: string) => void;
 }
 
-const InputPin: React.FC<Props> = ({ length = 4, className, inputStyle}) => {
+const InputPin: React.FC<Props> = ({
+  id,
+  value,
+  setValue,
+  type = 'text',
+  length = 4,
+  className,
+  inputStyle,
+}) => {
   const arrayRef = React.useRef<any>([]);
   const [values, setValues] = React.useState<any[]>(new Array(length).fill(''));
-
+  const [targetIndex, setTargetIndex] = React.useState(0);
 
   const handleChange = (index: number) => {
     return (e: any) => {
-      console.log(e.target.value)
       setValues((prev: any[]) => {
         return prev.map((item, i) => {
           if (i === index) {
             return e.target.value.slice(-1);
           }
           return item;
-        })
+        });
       });
       if (e.target.value) {
-        arrayRef.current[index+1]?.focus();
+        const nextIndex = index + 1;
+        const tIndex = nextIndex >= length ? length - 1 : nextIndex;
+        setTargetIndex(tIndex);
+        if (nextIndex >= length) {
+          arrayRef.current[tIndex]?.blur();
+        } else {
+          arrayRef.current[tIndex]?.focus();
+        }
       }
-    }
-  }
-  const handleBackspace = (index: number) => {
-    console.log(values);
-    arrayRef.current[index-1]?.focus();
-  }
+    };
+  };
 
-  const handleKeyUp = (index: number) =>  (e: any) => {
-    console.log(e.keyCode);
+  React.useEffect(() => {
+    if (values.length) {
+      setValue?.(values.join(''));
+    }
+  }, [values]);
+  const handleBackspace = (index: number) => {
+    const tIndex = index - 1 < 0 ? 0 : index - 1;
+    arrayRef.current[tIndex]?.focus();
+    setTargetIndex(tIndex);
+  };
+
+  const handleKeyUp = (index: number) => (e: any) => {
     if (e.keyCode === 8) {
       handleBackspace(index);
     }
-  }
-  return <label className={className}>
-    {
-      values.map((v, i) => {
-        return <input style={inputStyle} key={i} onKeyUp={handleKeyUp(i)} ref={el => arrayRef.current[i] = el} type="text" value={v} onChange={handleChange(i)} />
-      })
-    }
-  </label>
-}
+  };
+  const forceFocus = (e: any) => {
+    arrayRef.current[targetIndex]?.focus();
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  return (
+    <label className={className} htmlFor={`${id}-${targetIndex}`}>
+      {values.map((v, i) => {
+        return (
+          <input
+            onClick={forceFocus}
+            id={`${id}-${i}`}
+            placeholder={' '}
+            style={inputStyle}
+            key={i}
+            onKeyUp={handleKeyUp(i)}
+            ref={(el) => (arrayRef.current[i] = el)}
+            type={type}
+            value={v}
+            onChange={handleChange(i)}
+          />
+        );
+      })}
+    </label>
+  );
+};
 export default InputPin;
